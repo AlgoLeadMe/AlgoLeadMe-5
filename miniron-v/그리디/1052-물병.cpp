b265ff73b486b348c2208d2b81ca05d1388b4ce9
miniron_v <61517039+miniron-v@users.noreply.github.com>
2024-01-10 03:31:40 +0900

    Update 1052-물병.cpp

--- miniron-v/그리디/1052-물병.cpp
+++ miniron-v/그리디/1052-물병.cpp
@@ -6,17 +6,28 @@ int main() {
 	int n, k;
 	std::cin >> n >> k;
 
+	if (k > n) {
+		std::cout << 0;
+		return 0;
+	}
+
 	// 최대한 많은 물병을 써서 k - 1개의 물병을 만든다.
-	for (int i = 0; i < k - 1; i++) {
+	while(--k) {
 		int bottles = 1;
 		for (; bottles * 2 < n; bottles *= 2);
 
 		n -= bottles;
+
+		if (n == 0) {
+			std::cout << 0;
+			return 0;
+		}
 	}
 
 	// 남은 n개의 물병보다 큰, 가장 작은 2의 지수승을 구한다.
 	int need = 1;
-	for (;  need < n; need *= 2);
+	for (; need < n; need *= 2);
 
 	std::cout << need - n;
+	return 0;
 }
