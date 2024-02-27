7831706a3cb88d88c82cf37132a3919ae1f8e682
bomik0221 <bomik0221@gmail.com>
2024-02-28 01:44:37 +0900

    240227 괄호 끼워넣기

---
+++ bomik0221/스택/240227.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <stack>
+
+int main() {
+	std::string S;
+	std::cin >> S;
+	std::stack<int>stack;
+
+	for (int num = 0; num < S.size(); num++) {
+		//if (num !=0 && S[num - 1] == '(' && S[num] == ')')	stack.pop();
+		if (!stack.empty() && stack.top() == 1 && S[num] == ')') stack.pop();
+		else {
+			if (S[num] == '(')	stack.push(1);
+			else stack.push(2);
+		}
+	}
+	std::cout << stack.size();
+	return 0;
+}
