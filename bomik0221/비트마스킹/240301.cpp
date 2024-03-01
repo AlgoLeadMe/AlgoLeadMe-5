deea9d20f26a94628d46c8b60924ba3f22c0fc42
bomik0221 <bomik0221@gmail.com>
2024-03-02 00:09:49 +0900

    240301 거듭제곱

---
+++ bomik0221/비트마스킹/240301.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+	long long n, sum = 0, s=1;	std::cin >> n;
+	
+
+	while (n) {
+		if (n & 1) {
+			sum += s;
+		}
+		s *= 3; //3의 거듭제곱
+		n >>= 1;	//n을 오른쪽으로 한 자리 밀어서 저장
+	}
+	std::cout << sum;
+	return 0;
+}
